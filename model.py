67532ef48b44b9e1ff475699122294036e8dcdbe
Thi2037 <148565451+Thi2037@users.noreply.github.com>
2025-02-13 13:46:57 +0530

    Update setting.py

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Task_manager_application',  # Database name
         'USER': 'your_mysql_user',  # Your MySQL username
-        'PASSWORD': 'your_mysql_password',  # Your MySQL password
+        'PASSWORD': 'your_mysql_password',  # Your MySQL password 
         'HOST': 'localhost',  # Change if MySQL is on a different host
         'PORT': '3306',  # MySQL default port
     }
